baab54fc4678876c7be3e33e2add34318c493380
hoorihoori <lovegodks@hanmail.net>
2025-09-19 13:18:39 +0900

    chore: update dependencies and add @storybook/addon-designs
    - Added @figspec/components and @figspec/react packages with their respective versions.
    - Updated @lit-labs/react, @lit-labs/ssr-dom-shim, and @lit/reactive-element dependencies.
    - Removed unnecessary rollup packages for Linux.
    - Added @types/trusted-types for type definitions.
    - Updated lit, lit-element, and lit-html packages to the latest versions.
    - Added design parameter to Button story for Figma integration.

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -7,9 +7,13 @@ export default {
   title: 'Example/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
-  },
+  // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+  layout: 'centered',
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File'
+  }
+},
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
